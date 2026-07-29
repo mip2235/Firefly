77ea75a4bac7d8b5759475c1b9d7c0a57d2666a2
mip2235 <128234196+mip2235@users.noreply.github.com>
2026-07-29 20:45:48 +0800

    1

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -102,7 +102,7 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 			},
 			{
 				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
+				url: "https://qm.qq.com/cgi-bin/qm/qr?k=z4Zb6CY-Hz2iYsjl6eIM1ezl3gXPJ8s1&jump_from=webapi&authKey=OnZppNnKI1BSyeRcEq8DQJHkhbwjtbvz8YxKVUZVPlZRCbHhz+n9MkUWEF4wxI98",
 				external: false,
 				icon: "fa7-brands:qq",
 			},
