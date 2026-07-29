70fed57358194a5d8f96c0dcea714e480b9e130c
mip2235 <128234196+mip2235@users.noreply.github.com>
2026-07-29 19:00:50 +0800

    1

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "",
+	title: "哈！(◣ ‸ ◢)",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "QWQ",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
